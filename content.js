4a2985b3af003f732f739d58cdbb4adec28e4b30
stiven.qehaja <stivenqehaja@gmail.com>
2025-03-29 01:06:13 +0100

    Hoverable Wheel

--- content.js
+++ content.js
@@ -34,6 +34,7 @@ function createEmoteWheel() {
     justify-content: center;
     align-items: center;
     color: white;
+    user-select: none;
   `;
   emoteWheel.innerHTML = `
     <div style="
@@ -44,68 +45,77 @@ function createEmoteWheel() {
       height: 100%;
       transform: rotate(45deg);
     ">
-      <div class="shortcut" style="grid-column: 1;
-        background: rgba(255,255,255,0.1);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-top-left-radius: 100%;
-        border-bottom: 3px solid rgb(75, 75, 75);
-        border-right: 3px solid rgb(75, 75, 75)">
-          <span style="
-            transform: rotate(-45deg);">Top</span></div>
+      <div class="shortcut" id="top"
+        style="
+          grid-column: 1;
+          background: rgb(40, 42, 48);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border-top-left-radius: 100%;
+          border-bottom: 3px solid rgb(75, 75, 75);
+          border-right: 3px solid rgb(75, 75, 75)">
+            <span style="transform: rotate(-45deg);">Top</span>
+      </div>
 
-      <div class="shortcut" style="grid-column: 2;
-        grid-row: 1;
-        background: rgba(255,255,255,0.1);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-top-right-radius: 100%;
-        border-left: 3px solid rgb(75, 75, 75);
-        border-bottom: 3px solid rgb(75, 75, 75)">
-          <span style="
-            transform: rotate(-45deg);">Right</span></div>
+      <div class="shortcut" id="right"
+        style="
+          grid-column: 2;
+          grid-row: 1;
+          background: rgb(40, 42, 48);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border-top-right-radius: 100%;
+          border-left: 3px solid rgb(75, 75, 75);
+          border-bottom: 3px solid rgb(75, 75, 75)">
+            <span style="transform: rotate(-45deg);">Right</span>
+      </div>
 
-      <div class="shortcut" style="grid-column: 1;
-        grid-row: 2;
-        background: rgba(255,255,255,0.1);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-bottom-left-radius: 100%;
-        border-right: 3px solid rgb(75, 75, 75);
-        border-top: 3px solid rgb(75, 75, 75)">
-          <span style="
-            transform: rotate(-45deg);">Left</span></div>
+      <div class="shortcut" id="left"
+        style="
+          grid-column: 1;
+          grid-row: 2;
+          background: rgb(40, 42, 48);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border-bottom-left-radius: 100%;
+          border-right: 3px solid rgb(75, 75, 75);
+          border-top: 3px solid rgb(75, 75, 75)">
+            <span style=" transform: rotate(-45deg);">Left</span>
+      </div>
 
-      <div class="shortcut" style="grid-column: 2;
-        grid-row: 2;
-        background: rgba(255,255,255,0.1);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-bottom-right-radius: 100%;
-        border-top: 3px solid rgb(75, 75, 75);
-        border-left: 3px solid rgb(75, 75, 75)">
-          <span style="
-            transform: rotate(-45deg);">Bottom</span></div>
+      <div class="shortcut" id="bottom"
+        style="
+          grid-column: 2;
+          grid-row: 2;
+          background: rgb(40, 42, 48);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border-bottom-right-radius: 100%;
+          border-top: 3px solid rgb(75, 75, 75);
+          border-left: 3px solid rgb(75, 75, 75)">
+            <span style="transform: rotate(-45deg);">Bottom</span>
+      </div>
 
-      <div class="shortcut" style="
-        z-index: 9999;
-        position: absolute;
-        top: 40%;
-        left: 40%;
-        height: 60px;
-        width: 60px;
-        background: rgb(40, 42, 48);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 100%;
-        border: 5px solid rgb(75, 75, 75);">
-          <span style="
-            transform: rotate(-45deg);">Center</span></div>
+      <div class="shortcut" id="center"
+        style="
+          z-index: 9999;
+          position: absolute;
+          top: 40%;
+          left: 40%;
+          height: 60px;
+          width: 60px;
+          background: rgb(40, 42, 48);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border-radius: 100%;
+          border: 5px solid rgb(75, 75, 75);">
+            <span style="transform: rotate(-45deg);">Cancel</span>
+        </div>
     </div>
   `;
   document.body.appendChild(emoteWheel);
@@ -119,11 +129,38 @@ function removeEmoteWheel() {
 }
 
 document.addEventListener("keydown", (event) => {
-  if (event.key === "F24") {
+  if (event.key === "F24" || event.key === "F7") {
     if (!isWheelActive) {
       console.log("Emote Wheel Activated");
       createEmoteWheel();
-      isWheelActive = true;
+
+      const emoteWheel = document.querySelector('.emote-wheel');
+      
+      if (!emoteWheel) {
+        console.error("Element '.emote-wheel' not found!");
+        return;
+      }
+
+      const shortcuts = emoteWheel.querySelectorAll('.shortcut');
+      shortcuts.forEach((shortcut) => {
+        shortcut.addEventListener('mouseover', () => {
+          console.log('Inside ' + shortcut.id);
+          shortcut.style.backgroundColor = 'rgb(108, 108, 108)';
+        });
+
+        isWheelActive = true;
+        shortcut.addEventListener('mouseleave', () => {
+          console.log('Outside ' + shortcut.id);
+          shortcut.style.backgroundColor = 'rgb(40, 42, 48)';
+        });
+
+        shortcut.addEventListener('contextmenu', (event) => {
+          console.log('Right-Click on Active Wheel');
+          event.preventDefault();          
+        });
+      });
+      
+
     }
   } else if (isWheelActive) {
     // Deactivate if any other key is pressed
@@ -134,7 +171,7 @@ document.addEventListener("keydown", (event) => {
 });
 
 document.addEventListener("keyup", (event) => {
-  if (event.key === "F24") {
+  if (event.key === "F24" || event.key === "F7") {
     console.log("Emote Wheel Deactivated (F24 released)");
     removeEmoteWheel();
     isWheelActive = false;
